5bc7f66e6fc554af57be8f67eb11e57543222aff
Avinash Sinha <savinash2608@gmail.com>
2020-09-17 15:57:54 +0530

    .env deleted

--- server.js
+++ server.js
@@ -6,18 +6,12 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-// App
 const app = express()
-
-// Settings
 app.disable('x-powered-by')
-
-// Middlewares
 app.use(express.json())
 app.use(cors())
 app.use('/api/v1', router)
 
-// Listen to port
 const PORT = process.env.PORT || 2608
 app.listen(
     PORT,
